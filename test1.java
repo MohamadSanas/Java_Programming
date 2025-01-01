7423e885637a1d9f2a1264cc71db211725d557ba
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-01-01 22:12:31 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,8 @@
+class test1{
+    public static void
+    main(String[] args){
+        int age =25,age1=45;
+        System.out.println("My age is "+age);
+        System.out.println("Her age is "+age1);
+    }
+}
