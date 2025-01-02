cbe0ac21414c2fe64af22152feb04cd7d7e766c2
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-01-02 22:43:11 +0530

    Add files via upload
    familiarize with creating object and class

---
+++ task6.java
@@ -0,0 +1,17 @@
+class task6{
+    void greet(){
+        System.out.println("hello");
+        dumy();
+
+    }
+
+    void dumy(){
+        System.out.println("iron man");
+    }
+
+    public static void main(String[] args){
+        task6 obj1 = new task6();
+        obj1.greet(); 
+    }
+
+}
